cf51cdc4995f7450874dce97ad7928d54bd3389d
Maxim Kazakov <kazakov.java@gmail.com>
2022-03-22 10:50:24 +0300

    15 Написана реализация ДЗ

--- war_of_tanks/src/main/java/com/otus/solid/first/war/of/tanks/controller/Message.java
+++ war_of_tanks/src/main/java/com/otus/solid/first/war/of/tanks/controller/Message.java
@@ -1,6 +1,7 @@
 package com.otus.solid.first.war.of.tanks.controller;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
@@ -9,17 +10,26 @@ import java.util.Map;
 /**
  * Сообщение, которое отправляет Агент игровому серверу
  * <p>
- * id игры - для идентификации игры, в рамках которой это сообщение обработано. С помощью этого id можно будет определить получателя сообщения при маршрутизации сообщения внутри игрового сервера.
- * id игрового объекта, которому адресовано сообщение. С помощью этого id можно будет определить игровой объект внутри игры, для которого адресовано это сообщение.
- * id операции - по этому id в IoC можно будет определить команду, которая будет обрабатывать данное сообщение.
- * args - вложенный json объект с параметрами операции. Содержимое этого объекта полностью зависит от команды, которая будет применяться к игровому объекту.
+ * scopeId - для идентификации игры, в рамках которой это сообщение обработано. С помощью этого id можно будет определить получателя сообщения при маршрутизации сообщения внутри игрового сервера.
+ * gameObjectId - id игрового объекта, которому адресовано сообщение. С помощью этого id можно будет определить игровой объект внутри игры, для которого адресовано это сообщение.
+ * operationId - id операции - по этому id в IoC можно будет определить команду, которая будет обрабатывать данное сообщение.
+ * varargs - вложенный json объект с параметрами операции. Содержимое этого объекта полностью зависит от команды, которая будет применяться к игровому объекту.
  */
 
+@Getter
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class Message {
-    long gameId;
-    long gameObjectId;
-    long operationId;
-    Map<String, String> args;
+    String scopeId;
+    String gameObjectId;
+    String operationId;
+    Map<String, Object> varargs;
+
+
+    public Map<String, Object> getVarargsForIoC() {
+        varargs.put("dependencyName", operationId);
+        varargs.put("scopeId", scopeId);
+        varargs.put("gameObjectId", gameObjectId);
+        return varargs;
+    }
 }
--- war_of_tanks/src/main/java/com/otus/solid/first/war/of/tanks/controller/MessageControllerImpl.java
+++ war_of_tanks/src/main/java/com/otus/solid/first/war/of/tanks/controller/MessageControllerImpl.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageControllerImpl implements MessageController {
+
     @Override
     public void executeMessage(Message message) {
 
