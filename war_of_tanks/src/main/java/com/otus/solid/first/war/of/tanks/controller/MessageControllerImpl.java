64453530c0d68cf73e944ddfe89ceba4c244e128
Maxim Kazakov <kazakov.java@gmail.com>
2022-03-22 10:55:27 +0300

    15 Доработка контроллера

--- war_of_tanks/src/main/java/com/otus/solid/first/war/of/tanks/controller/MessageControllerImpl.java
+++ war_of_tanks/src/main/java/com/otus/solid/first/war/of/tanks/controller/MessageControllerImpl.java
@@ -1,12 +1,20 @@
 package com.otus.solid.first.war.of.tanks.controller;
 
+import com.otus.solid.first.war.of.tanks.actions.Action;
+import com.otus.solid.first.war.of.tanks.iocResolvers.IoC;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class MessageControllerImpl implements MessageController {
 
     @Override
     public void executeMessage(Message message) {
-
+        Action action = IoC.resolve(Map.of(
+                "dependencyName", "InterpretCommand",
+                "scopeId", message.getScopeId(),
+                "message", message));
+        action.execute();
     }
 }
